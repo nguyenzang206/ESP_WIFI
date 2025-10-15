2cc9e861a0f9dcd48f0bfd5974f154163d86c6bc
nguyenzang206 <Vannguyentrong262728@gmail.com>
2025-10-15 10:11:57 +0700

    Delete bài tập directory

--- bài tập/Untitled-1.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    #Phần neo
-    if n == 0 or n == 1:
-        return 1
-    #phần đệ quy
-    else:
-        return n * factorial(n-1)
-
-print n = 4
--- bài tập/luythua.py
+++
@@ -1,9 +0,0 @@
-def luy_thua(a, n):
-    if n == 0:
-        return 1
-    else:
-        return a * luy_thua(a, n-1)
-    
-a = int(input("Nhập một số nguyên không âm: "))
-n = float(input("Nhập một số bất kì: "))
-print(luy_thua(a, n))
